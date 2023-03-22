31f49e3f5d5a2ad4a56fc871dd38f70b9f0cdc3c
alemonacoo <ale.monaco@outlook.com>
2023-03-22 12:49:23 +0100

    adding app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
 import Header from "./components/Header";
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
     <div>
-      <Header text="Twitter mock-up - React Test 2" />
+      <Header text="Fake Twitter - React Test 2" />
+      <div className="profileCardsContainer">
+        <ProfileCard
+          profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png?20220125121207"
+          user="@react237"
+          post="i'm dumb"
+        />
+        <ProfileCard
+          profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1245px-Twitter-logo.svg.png"
+          user="@twitter01"
+          post="i'm dumb"
+        />
+        <ProfileCard
+          profilePic="https://www.facebook.com/images/fb_icon_325x325.png"
+          user="@facebook69"
+          post="i'm dumb"
+        />
+        <ProfileCard
+          profilePic="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          user="@linkedin420"
+          post="i'm dumb"
+        />
+      </div>
     </div>
   );
 }
